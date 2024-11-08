c5d5388c40c7ad40511b6d673c23920e2a639157
sumuduFdo <sumudumf@gmail.com>
2024-11-08 17:13:39 +0530

    added index page

--- src/components/pages/Index.tsx
+++ src/components/pages/Index.tsx
@@ -6,22 +6,12 @@ import { useState } from 'react';
 const Index = () => {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const onLogin = () => {
-    setIsLoggedIn(true);
-  }
  
-  const onLogout = () => {
-    localStorage.removeItem('userId');
-    localStorage.removeItem('authToken');
-    setIsLoggedIn(false);
-  }
-
   return (
     <>
-      <Header isloggedIn={isLoggedIn} onLogout={onLogout}/>
+      <Header isloggedIn={isLoggedIn} setLoggedIn={setIsLoggedIn}/>
       <main className='main'>
-        <Outlet context={setIsLoggedIn}/>
+        <Outlet context={{isLoggedIn, setIsLoggedIn}}/>
       </main>
     </>
   );
