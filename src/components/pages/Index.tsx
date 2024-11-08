5cc122631fe849a5d9d9b03d26845d8ea086f8ba
sumuduFdo <sumudumf@gmail.com>
2024-11-08 22:28:54 +0530

    added auto validation on index page

--- src/components/pages/Index.tsx
+++ src/components/pages/Index.tsx
@@ -1,20 +1,34 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from "react-router-dom";
 
-import Header from '../Header';
-import { useState } from 'react';
+import Header from "../Header";
+import { useEffect, useState } from "react";
 
 const Index = () => {
-
   const [isLoggedIn, setIsLoggedIn] = useState(false);
- 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const auth = localStorage.getItem("shipmentAuth");
+    if (auth) {
+      const tokenData = JSON.parse(auth).authToken;
+      if (tokenData) {
+        navigate("/shipments");
+      } else {
+        navigate("/login");
+      }
+    } else {
+      navigate("/login");
+    }
+  }, []);
+
   return (
     <>
-      <Header isloggedIn={isLoggedIn} setLoggedIn={setIsLoggedIn}/>
-      <main className='main'>
-        <Outlet context={{isLoggedIn, setIsLoggedIn}}/>
+      <Header isloggedIn={isLoggedIn} setLoggedIn={setIsLoggedIn} />
+      <main className="main">
+        <Outlet context={{ isLoggedIn, setIsLoggedIn }} />
       </main>
     </>
   );
-}
+};
 
 export default Index;
