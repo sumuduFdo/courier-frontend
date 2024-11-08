ddf8473b42c57ca7e63831c5277ca60be352bdd1
sumuduFdo <sumudumf@gmail.com>
2024-11-08 22:30:04 +0530

    added form validation for user registration

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -2,6 +2,16 @@ import { Form } from "react-router-dom";
 import forms from "./shared/FormStyles.module.css";
 
 const Register = () => {
+
+  function getCurrentDate() {
+    const date = new Date;
+    const year: number = date.getFullYear()
+    const month: number = date.getMonth() + 1;
+    const day: number = date.getDate();
+
+    return `${year}-${month}-${day}`;
+  }
+
   return (
     <Form className={forms.form} method="POST">
       <div className={forms.formwrapper}>
@@ -10,17 +20,38 @@ const Register = () => {
         <div className={forms.formgroupsection}>
           <div className={forms.formgroup}>
             <label htmlFor="firstName">First Name</label>
-            <input type="text" name="firstName" id="firstName" required />
+            <input
+              type="text"
+              name="firstName"
+              pattern="^[a-zA-Z._ ]*$"
+              maxLength={30}
+              id="firstName"
+              required
+            />
           </div>
           <div className={forms.formgroup}>
             <label htmlFor="lastName">Last Name</label>
-            <input type="text" name="lastName" id="lastName" required />
+            <input
+              type="text"
+              name="lastName"
+              id="lastName"
+              pattern="^[a-zA-Z._ ]*$"
+              maxLength={30}
+              required
+            />
           </div>
         </div>
         <div className={forms.formgroupsection}>
           <div className={forms.formgroup}>
             <label htmlFor="dateOfBirth">Date of Birth</label>
-            <input type="date" name="dateOfBirth" id="dateOfBirth" required />
+            <input
+              type="date"
+              name="dateOfBirth"
+              min="1900-01-01"
+              max={`${getCurrentDate()}`}
+              id="dateOfBirth"
+              required
+            />
           </div>
 
           <div className={forms.formgroup}>
@@ -35,8 +66,8 @@ const Register = () => {
             <input type="email" name="email" id="email" required />
           </div>
           <div className={forms.formgroup}>
-            <label htmlFor="confimEmail">Confirm Email</label>
-            <input type="email" name="confimEmail" id="confimEmail" required />
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" required />
           </div>
         </div>
         <div className={forms.formgroupsection}>
@@ -46,6 +77,8 @@ const Register = () => {
               type="houseNumber"
               name="houseNumber"
               id="houseNumber"
+              pattern="^[0-9A-Z/ ]*$"
+              maxLength={10}
               required
             />
           </div>
@@ -55,6 +88,8 @@ const Register = () => {
               type="text"
               name="streetAddress"
               id="streetAddress"
+              pattern="^[a-zA-Z0-9,. ]*$"
+              maxLength={400}
               required
             />
           </div>
@@ -62,32 +97,30 @@ const Register = () => {
         <div className={forms.formgroupsection}>
           <div className={forms.formgroup}>
             <label htmlFor="city">City</label>
-            <input type="text" name="city" id="city" required />
+            <input
+              type="text"
+              name="city"
+              id="city"
+              pattern="^[A-Za-zÀ-ÿ\u00C0-\u00FF\s'-]+$"
+              maxLength={100}
+              required
+            />
           </div>
 
           <div className={forms.formgroup}>
             <label htmlFor="zipcode">Zipcode</label>
-            <input type="text" name="zipcode" id="zipcode" required />
-          </div>
-        </div>
-        <div className={forms.formgroupsection}>
-          <div className={forms.formgroup}>
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" required />
-          </div>
-
-          <div className={forms.formgroup}>
-            <label htmlFor="confirmPassword">Confirm Password</label>
             <input
-              type="password"
-              name="confirmPassword"
-              id="confirmPassword"
+              type="text"
+              name="zipcode"
+              id="zipcode"
+              pattern="^[0-9\s\-]{3,10}$"
+              maxLength={20}
               required
             />
           </div>
         </div>
 
-        <div className={forms.formgroup} style={{'marginTop': '1.5rem'}}>
+        <div className={forms.formgroup} style={{ marginTop: "1.5rem" }}>
           <button
             type="submit"
             className={`${forms.submitbtn} ${forms.registerbtn}`}
