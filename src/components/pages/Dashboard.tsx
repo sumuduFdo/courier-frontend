05b945837369f2b984c765873891d9b4a921af6e
sumuduFdo <sumudumf@gmail.com>
2024-11-08 17:14:13 +0530

    refactor dashboard component, add auth validation on component load

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -1,11 +1,43 @@
-import { useOutletContext } from "react-router-dom";
-import Dashboard from "../Dashboard";
+import { Outlet, useOutletContext, useNavigate } from "react-router-dom";
+import React from "react";
 
-function DashboardPage() {
+export const DashboardPage = () => {
+  const { setIsLoggedIn } = useOutletContext() as {setIsLoggedIn: any};
+  const navigate = useNavigate();
 
-    
 
-    return <Dashboard />
-}
+  React.useEffect(() => {
+    validateUserLogin();
+  }, []);
+
+  const getAuthToken = () => {
+    let userId = "";
+    let authToken = "";
+    const authInfo = localStorage.getItem("shipmentAuth");
+    if (authInfo) {
+      const userAuthInfo = JSON.parse(authInfo);
+      userId = userAuthInfo.userId;
+      authToken = userAuthInfo.authToken;
+    }
+
+    return { userId: userId, token: authToken };
+  };
+  const validateUserLogin = () => {
+    const { token } = getAuthToken();
+    if (token !== "") {
+      setIsLoggedIn(true);
+    } else {
+      navigate("/login");
+    }
+  };
+
+  return (
+    <>
+      <main className="main">
+        <Outlet />
+      </main>
+    </>
+  );
+};
 
 export default DashboardPage;
