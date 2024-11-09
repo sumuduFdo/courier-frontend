9889a86043c6b04ef447f9c2afdad17c3ae599d6
sumuduFdo <sumudumf@gmail.com>
2024-11-09 12:27:56 +0530

    added index page with routing

---
+++ src/components/Index.tsx
@@ -0,0 +1,43 @@
+import { Outlet, useLoaderData, useNavigate } from "react-router-dom";
+
+import Header from "./Header";
+import { useEffect, useState } from "react";
+import { getToken, getTokenDuration } from "./util/authLoader";
+import { logoutHandler } from "./util/lougoutHandler";
+
+const Index = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = getToken();
+    if (token) {
+      navigate("/shipments");
+    } else {
+      setIsLoggedIn(false);
+      navigate("/login");
+    }
+  }, [navigate]);
+
+  const token = useLoaderData();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+    const remainingDuration = getTokenDuration();
+    setTimeout(() => {
+      logoutHandler(setIsLoggedIn, navigate);
+    }, remainingDuration);
+  }, [navigate, token]);
+
+  return (
+    <>
+      <Header isloggedIn={isLoggedIn} setLoggedIn={setIsLoggedIn} />
+      <main className="main">
+        <Outlet context={{ isLoggedIn, setIsLoggedIn }} />
+      </main>
+    </>
+  );
+};
+
+export default Index;
